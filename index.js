a71e8ec2998e9f317ab81b2e953d388e14a68f53
Joonas Iivonen <joonas.iivonen@bc.fi>
2024-01-18 11:45:34 +0200

    stylefixes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 const port = 3000;
 const host = 'localhost';
 
-app.use('/img',express.static('kuvat'));
-app.use('/includes',express.static('includes'));
+app.use('/img', express.static('kuvat'));
+app.use('/includes', express.static('includes'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'templates/index.html')));
 
